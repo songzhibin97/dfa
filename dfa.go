b90222df3dbb16f1b554ffabbede775580dbe9ca
songzhibin97 <718428482@qq.com>
2022-08-07 11:13:18 +0800

    feat: dfa check

--- dfa.go
+++ dfa.go
@@ -2,6 +2,7 @@ package dfa
 
 import (
 	"errors"
+	"fmt"
 	"gopkg.in/yaml.v2"
 	"sync"
 )
@@ -16,6 +17,97 @@ type DFA struct {
 	effectiveState map[string]MetaStatus
 
 	start MetaStatus
+
+	ends []MetaStatus
+
+	diagnosis []string
+}
+
+// checkConnectivity 检查连通性
+func (d *DFA) checkConnectivity() error {
+	// 检查是否起点能到所有终点
+	var endMp = make(map[string]bool)
+	var stepMp = make(map[string]bool)
+	var unknownNode []string
+
+	var dfs func(node MetaStatus)
+	dfs = func(node MetaStatus) {
+		if node.ID == "" {
+			unknownNode = append(unknownNode, node.ID)
+			return
+		}
+		if stepMp[node.ID] {
+			return
+		}
+		stepMp[node.ID] = true
+
+		if node.FinalState {
+			endMp[node.ID] = true
+			return
+		}
+		for _, nid := range node.Next {
+			if nid == node.ID {
+				continue
+			}
+			dfs(d.effectiveState[nid])
+		}
+	}
+
+	dfs(d.start)
+
+	if len(unknownNode) != 0 {
+		return errors.New(fmt.Sprintf("unknown node: %v", unknownNode))
+	}
+	// 检查是否所有点都有用到(如果没用到 直接删了)
+	if len(endMp) != len(d.ends) {
+		unionSet := map[string]bool{}
+		for _, end := range d.ends {
+			unionSet[end.ID] = true
+		}
+		d.ends = d.ends[:0]
+		for key := range endMp {
+			d.ends = append(d.ends, d.effectiveState[key])
+			delete(unionSet, key)
+		}
+		unionList := make([]string, 0, len(unionSet))
+		for s := range unionSet {
+			unionList = append(unionList, s)
+			delete(d.effectiveState, s)
+		}
+		d.diagnosis = append(d.diagnosis, fmt.Sprintf("not all end nodes are reachable: %v", unionList))
+	}
+
+	if len(stepMp) != len(d.effectiveState) {
+		unionList := make([]string, 0, len(d.effectiveState)-len(stepMp))
+		for key := range d.effectiveState {
+			if !stepMp[key] {
+				unionList = append(unionList, key)
+				delete(d.effectiveState, key)
+			}
+		}
+		d.diagnosis = append(d.diagnosis, fmt.Sprintf("not all nodes are reachable: %v", unionList))
+	}
+
+	return nil
+}
+
+func (d *DFA) checkNodeEffective(node MetaStatus) error {
+	if node.ID == "" {
+		return errors.New("node id is empty")
+	}
+
+	if node.InitialState && len(node.Next) == 0 {
+		return errors.New("initial state must have next")
+	}
+
+	if node.FinalState && len(node.Next) != 0 {
+		return errors.New("final state must not have next")
+	}
+
+	if !node.FinalState && len(node.Next) == 0 {
+		return errors.New("non-final state must have next")
+	}
+	return nil
 }
 
 func (d *DFA) Get(key string) *MetaStatus {
@@ -35,21 +127,35 @@ func NewDfa(config string) (*DFA, error) {
 	if err != nil {
 		return nil, err
 	}
-	var end bool
 	for _, v := range _status {
-		if v.InitialState {
+
+		err = ret.checkNodeEffective(v)
+		if err != nil {
+			return nil, err
+		}
+
+		// check 检查 是否是一个有效的状态节点
+		if v.InitialState && len(v.Next) != 0 {
+			if ret.start.ID != "" {
+				return nil, errors.New("duplicate start state")
+			}
 			ret.start = v
 		}
 		if v.FinalState {
-			end = true
+			// 终态的可执行转态必须为空
+			ret.ends = append(ret.ends, v)
 		}
 		ret.effectiveState[v.ID] = v
 	}
+
 	if ret.start.ID == "" {
 		return nil, errors.New("no start state")
 	}
-	if !end {
+
+	if len(ret.ends) == 0 {
 		return nil, errors.New("no final state")
 	}
-	return ret, nil
+	// 检查从 start 到 end 的路径是否存在
+
+	return ret, ret.checkConnectivity()
 }
--- dfa_test.go
+++ dfa_test.go
@@ -1,6 +1,7 @@
 package dfa
 
 import (
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"testing"
 )
@@ -16,4 +17,122 @@ func TestNewDfa(t *testing.T) {
 		t.Error(err)
 	}
 	t.Log(dfa.effectiveState)
+}
+
+func TestDFA_checkConnectivity(t *testing.T) {
+	tests := []struct {
+		config    string
+		err       bool
+		diagnosis int
+	}{
+		{
+			config: `
+- id: "start"
+  payload: "start"
+  next:
+    - "1"
+  initial_state: true
+
+- id: "1"
+  payload: "1"
+  next:
+    - "2"
+
+- id: "2"
+  payload: "2"
+  next:
+    - "3"
+
+- id: "3"
+  payload: "3"
+  next:
+    - "end"
+
+- id: "end"
+  payload: "end"
+  final_state: true
+
+- id: "end1"
+  payload: "end1"
+  final_state: true		
+
+- id: "end2"
+  payload: "end2"
+  final_state: true			
+`,
+			err:       false,
+			diagnosis: 1,
+		},
+		{
+			config: `
+- id: "start"
+  payload: "start"
+  next:
+    - "1"
+  initial_state: true
+
+- id: "1"
+  payload: "1"
+  next:
+    - "2"
+
+- id: "2"
+  payload: "2"
+  next:
+    - "3"
+
+- id: "3"
+  payload: "3"
+  next:
+    - "end"
+
+- id: "4"
+  payload: "4"
+  next:
+    - "end"			
+
+- id: "end"
+  payload: "end"
+  final_state: true
+`,
+			err:       false,
+			diagnosis: 1,
+		},
+		{
+			config: `
+- id: "start"
+  payload: "start"
+  next:
+    - "1"
+  initial_state: true
+
+- id: "1"
+  payload: "1"
+  next:
+    - "2"
+
+- id: "2"
+  payload: "2"
+  next:
+    - "3"
+
+- id: "3"
+  payload: "3"
+  next:
+    - "4"
+    - "end"
+
+- id: "end"
+  payload: "end"
+  final_state: true
+`,
+			err:       true,
+			diagnosis: 0,
+		},
+	}
+	for _, tt := range tests {
+		d, err := NewDfa(tt.config)
+		assert.Equal(t, tt.err, err != nil)
+		assert.Equal(t, len(d.diagnosis), tt.diagnosis)
+	}
 }
